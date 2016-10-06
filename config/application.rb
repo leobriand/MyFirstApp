ffcce08ab8ec6bbea150572b6a33913cdcfc7bc6
leobriand <leo.briand@polytechnique.edu>
2016-10-06 16:13:26 +0200

    fin back 3 edition 2

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.middleware.insert_before 0, "Rack::Cors" do
-    allow do
-    	origins '*'
-	    resource '*', :headers => :any, :methods => [:get, :post, :options]
-    end
-end
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -18,5 +11,11 @@ module MyFirstApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, "Rack::Cors" do
+   		allow do
+    		origins '*'
+	    	resource '*', :headers => :any, :methods => [:get, :post, :options]
+   		 end
+	end
   end
 end
